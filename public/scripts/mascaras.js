b052594d9ba9e3633a8fa2a844632940bc861c09
Bruno Gomes <icestonebruno@gmail.com>
2021-06-28 10:37:10 -0300

    visual relatorio filtros

--- public/scripts/mascaras.js
+++ public/scripts/mascaras.js
@@ -136,3 +136,19 @@ function mascaraData(val) {
   return true;
 }
 
+
+function formatarMoeda(elemento) {
+  var valor = elemento.value;
+
+  valor = valor + '';
+  valor = parseInt(valor.replace(/[\D]+/g, ''));
+  valor = valor + '';
+  valor = valor.replace(/([0-9]{2})$/g, ",$1");
+
+  if (valor.length > 6) {
+      valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+  }
+  
+  elemento.value = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });;
+  if(valor == 'NaN') elemento.value = '';
+}
