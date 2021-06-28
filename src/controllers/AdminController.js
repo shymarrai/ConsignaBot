963400c21c84050435e300366a5ef0631882c5e4
Bruno Gomes <icestonebruno@gmail.com>
2021-06-28 12:48:04 -0300

    finally

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -10,7 +10,7 @@ var ano = data.getFullYear();
 dataAtual = dia + '/' + mes + '/' + ano;
 
 const ws = wb.addWorksheet(`${String(dataAtual)}`)
-const headingColumnsNames = ['Criado', 'Operador', 'Supervisor', 'Operacional', "Nome","CPF"	,"Data Nasc.",	"Endereço", "Bairro"	,"Cidade",	"UF",	"CEP",	"Contato1",	"Zap",	"Nº Benefício",	"Banco", "Salário",	"Agência",	"Tipo",	"Nº Conta",	"Tipo Conta", "Tipo de Operação"	,"Banco origem",	"Quitação",	"Valor Parcelas",	"Total Parcelas", "Parcelas restantes", "Parcela desejada", "Qtd Pagas", "Contrato", "Taxa", "Valor liberado" ,	"Status","Obs"]
+const headingColumnsNames = ['Criado','Supervisor', 'Operacional', "Nome","CPF",	"Nº Benefício","Tipo de Operação"	,"Banco Origem","Valor Parcela" ,"Quitação" ,	"Parcelas Restantes", "Contrato","Taxa","Status","Obs"]
 const Admin = {
   dash: async function (req, res) {
     const token = req.params.token
@@ -39,6 +39,10 @@ const Admin = {
     const de = req.body.de
     const ate = req.body.ate
 
+    const operacional = req.body.filtro_operacional
+    const status = req.body.filtro_status
+    const supervisor = req.body.filtro_supervisor
+
     const selectedUser = await User.findOne({ username })
 
     if (!token) return res.status(401).send("Acesso Negado")
@@ -47,15 +51,25 @@ const Admin = {
     if(!de) return res.redirect(`/relatorio/${selectedUser.username}/${token}`)
     if(!ate) return res.redirect(`/relatorio/${selectedUser.username}/${token}`)
 
+    // {"$and": [{cli_cpf  : cpfClient},{ url:  {$ne:''} }]}
+    try {  
+      const filtroOperacional =  (operacional !== 'todos') ? {"operacional": operacional} : {}
+      const filtroSupervisor =  (supervisor !== 'todos') ? {"supervisor": supervisor} : {}
+      const filtroStatus =  (status !== 'todos') ? {"status": status} : {}
+      
+      const result = await Client.find({"$and": [{"created_at": { "$gte" : de , "$lte" : ate}},filtroOperacional,filtroSupervisor,filtroStatus] }).sort({"_id" : -1})
 
-    try {
-      const result = await Client.find({"created_at": { "$gte" : de , "$lte" : ate}}).sort({"_id" : -1})
+
       if(result.length <= 0) return res.send(`Resultado Vazio <a href='/relatorio/${selectedUser.username}/${token}'>Voltar</a>`)
       
       let headingColumnsIndex = 1
       headingColumnsNames.forEach( heading => {
         ws.cell(1,headingColumnsIndex++).string(heading)
       })
+
+
+
+
       // ['Criado', 'Operador', 'Supervisor', "Nome","CPF"	,"Data Nasc.",
       // "Endereço", "Bairro"	,"Cidade",	"UF",	"CEP",	"Contato1",	"Contato2",	
       // "Nº Benefício",	"Banco",	"Agência",	"Tipo",	"Nº Conta",	"Tipo Conta",	
@@ -66,42 +80,20 @@ const Admin = {
       let rowIndex = 2;
       result.forEach(record => {
         ws.cell(rowIndex, 1).string(String(record['created_at']))        
-        ws.cell(rowIndex, 2).string(String(record['operador']))        
-        ws.cell(rowIndex, 3).string(String(record['supervisor']))        
-        ws.cell(rowIndex, 4).string(String(record['operacional']))        
-        ws.cell(rowIndex, 5).string(String(record['cli_nome']))        
-        ws.cell(rowIndex, 6).string(String(record['cli_cpf']))        
-        ws.cell(rowIndex, 7).string(String(record['cli_data_nasc']))        
-        ws.cell(rowIndex, 8).string(String(record['ENDERECO']))        
-        ws.cell(rowIndex, 9).string(String(record['BAIRRO']))        
-        ws.cell(rowIndex, 10).string(String(record['CIDADE']))        
-        ws.cell(rowIndex, 10).string(String(record['UF_ENDERECO']))        
-        ws.cell(rowIndex, 11).string(String(record['CEP']))        
-        ws.cell(rowIndex, 12).string(String(record['contato1']))        
-        ws.cell(rowIndex, 13).string(String(record['contato2']))        
-        ws.cell(rowIndex, 14).string(String(record['cli_matricula']))        
-        ws.cell(rowIndex, 15).string(String(record['Banco']))        
-        ws.cell(rowIndex, 15).string(String(record['rf_extra_base_margem']))        
-        ws.cell(rowIndex, 16).string(String(record['Agencia_Pagto']))        
-        ws.cell(rowIndex, 17).string(String(record['especie']))        
-        ws.cell(rowIndex, 18).string(String(record['contacorrente']))        
-        ws.cell(rowIndex, 19).string(String(record['meiopagto']))        
-        ws.cell(rowIndex, 19).string(String(record['tipo_operacao']))        
-        ws.cell(rowIndex, 20).string(String(record['banco_origem']))  /* banco origem  */     
-        ws.cell(rowIndex, 22).string(String(record['quitacao']))  /* quitacao */
-        ws.cell(rowIndex, 23).string(String(record['v_parcela']))  /* valor parcelas */
-        ws.cell(rowIndex, 24).string(String(record['total_parcelas']))  /* total parcelas */
-        ws.cell(rowIndex, 25).string(String(record['parcelas_restantes']))   /* parcelas restantes */
-        ws.cell(rowIndex, 26).string(String(record['qtd_pagas']))     /* qtd pagas */  
-        ws.cell(rowIndex, 26).string(String(record['desejada']))     /* desejada */  
-        ws.cell(rowIndex, 27).string(String(record['n_contrato']))     /* contrato */  
-        ws.cell(rowIndex, 28).string(String(record['taxa']))        /* taxa */       
-        ws.cell(rowIndex, 28).string(String(record['v_liberado']))        /* v_liberado */       
-        ws.cell(rowIndex, 29).string(String(record['status']))         /* status */
-        ws.cell(rowIndex, 22).string(String(record['margem']))  /* margem */
-        ws.cell(rowIndex, 22).string(String(record['parcela_nova']))  /* parcela_nova */
-        ws.cell(rowIndex, 22).string(String(record['valor_saque']))  /* valor_saque */      
-        ws.cell(rowIndex, 30).string(String(record['obs']))        /* obs */
+        ws.cell(rowIndex, 2).string(String(record['supervisor']))        
+        ws.cell(rowIndex, 3).string(String(record['operacional']))        
+        ws.cell(rowIndex, 4).string(String(record['cli_nome']))        
+        ws.cell(rowIndex, 5).string(String(record['cli_cpf']))        
+        ws.cell(rowIndex, 6).string(String(record['cli_matricula']))       /* n beneficio */        
+        ws.cell(rowIndex, 7).string(String(record['tipo_operacao']))        /*tipo de operacao  */
+        ws.cell(rowIndex, 8).string(String(record['banco_origem']))       /* banco origem  */
+        ws.cell(rowIndex, 9).string(String(record['v_parcela']))  /* valor parcelas */
+        ws.cell(rowIndex, 10).string(String(record['quitacao']))  /* quitacao */
+        ws.cell(rowIndex, 11).string(String(record['parcelas_restantes']))   /* parcelas restantes */
+        ws.cell(rowIndex, 12).string(String(record['n_contrato']))     /* contrato */  
+        ws.cell(rowIndex, 13).string(String(record['taxa']))        /* taxa */            
+        ws.cell(rowIndex, 14).string(String(record['status']))         /* status */  
+        ws.cell(rowIndex, 15).string(String(record['obs']))        /* obs */
         rowIndex++
       })
 
